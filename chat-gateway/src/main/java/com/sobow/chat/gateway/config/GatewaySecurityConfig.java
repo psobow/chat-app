6d4c0cb45c38403617e49c0f4fe8fcd77ed6275f
Patryk Sobow <patryk.sobow95@gmail.com>
2025-10-13 23:53:47 +0200

    Add gateway config

---
+++ chat-gateway/src/main/java/com/sobow/chat/gateway/config/GatewaySecurityConfig.java
@@ -0,0 +1,35 @@
+package com.sobow.chat.gateway.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+    
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(exchanges -> exchanges
+                       // All endpoints are secured
+                       .anyExchange()
+                       .authenticated()
+                   )
+                   // Configure OAuth2 Resource Server
+                   .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+                   // Disable CSRF
+                   .csrf(CsrfSpec::disable)
+                   .build();
+    }
+    
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        return new JwtAuthenticationConverter();
+    }
+}
