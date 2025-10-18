a75e4f55057b9b6e116bcfae82069a81ce18bafc
Patryk Sobow <patryk.sobow95@gmail.com>
2025-10-18 23:34:16 +0200

    Add DTO and Mapper

---
+++ chat-contact/src/main/java/com/sobow/chat/contact/mapper/ContactMapper.java
@@ -0,0 +1,14 @@
+package com.sobow.chat.contact.mapper;
+
+import com.sobow.chat.contact.domain.dto.ListContactResponseDto;
+import com.sobow.chat.contact.domain.entity.Contact;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ContactMapper {
+    
+    @Mapping(source = "contactUserId", target = "userId")
+    ListContactResponseDto toDto(Contact contact);
+}
