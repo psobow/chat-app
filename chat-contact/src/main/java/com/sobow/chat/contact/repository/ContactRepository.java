c533f1b17e1c7e28c6b7229535682ff15b66de2a
Patryk Sobow <patryk.sobow95@gmail.com>
2025-10-18 22:58:34 +0200

    Add ContactRepository

---
+++ chat-contact/src/main/java/com/sobow/chat/contact/repository/ContactRepository.java
@@ -0,0 +1,13 @@
+package com.sobow.chat.contact.repository;
+
+import com.sobow.chat.contact.domain.entity.Contact;
+import java.util.UUID;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface ContactRepository extends ReactiveCrudRepository<Contact, UUID> {
+    
+    Flux<Contact> findAllByOwnerId(UUID ownerId);
+}
