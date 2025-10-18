9cd55adaf9a935a95ffde1ae6ffbeadab9803a0f
Patryk Sobow <patryk.sobow95@gmail.com>
2025-10-18 21:35:30 +0200

    Create and setup contact service

---
+++ chat-contact/src/main/java/com/sobow/chat/contact/ChatContactApplication.java
@@ -0,0 +1,12 @@
+package com.sobow.chat.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatContactApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ChatContactApplication.class, args);
+    }
+}
