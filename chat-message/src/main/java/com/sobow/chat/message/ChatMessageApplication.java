665dcee1ec33946217130fd76fca959bf784fd94
Patryk Sobow <patryk.sobow95@gmail.com>
2025-10-27 18:14:28 +0100

    Add message microservice

---
+++ chat-message/src/main/java/com/sobow/chat/message/ChatMessageApplication.java
@@ -0,0 +1,12 @@
+package com.sobow.chat.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMessageApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ChatMessageApplication.class, args);
+    }
+}
