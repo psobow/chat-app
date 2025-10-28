7e81e38f5d8d72ff6d458084f6fb580f56948537
Patryk Sobow <patryk.sobow95@gmail.com>
2025-10-28 21:13:46 +0100

    Add getChatMessageHistory to service layer

--- chat-message/src/main/java/com/sobow/chat/message/service/ChatMessageService.java
+++ chat-message/src/main/java/com/sobow/chat/message/service/ChatMessageService.java
@@ -1,9 +1,13 @@
 package com.sobow.chat.message.service;
 
 import com.sobow.chat.message.domain.entity.ChatMessage;
+import java.util.UUID;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ChatMessageService {
     
     Mono<ChatMessage> save(ChatMessage chatMessage);
+    
+    Flux<ChatMessage> getChatMessageHistory(UUID currentUserId, UUID contactUserId);
 }
--- chat-message/src/main/java/com/sobow/chat/message/service/impl/ChatMessageServiceImpl.java
+++ chat-message/src/main/java/com/sobow/chat/message/service/impl/ChatMessageServiceImpl.java
@@ -4,8 +4,10 @@ import com.sobow.chat.message.domain.entity.ChatMessage;
 import com.sobow.chat.message.repository.ChatMessageRepository;
 import com.sobow.chat.message.service.ChatMessageService;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -20,5 +22,10 @@ public class ChatMessageServiceImpl implements ChatMessageService {
         chatMessage.setCreatedAt(now);
         chatMessage.setUpdatedAt(now);
         return chatMessageRepository.save(chatMessage);
+    }
+    
+    @Override
+    public Flux<ChatMessage> getChatMessageHistory(UUID currentUserId, UUID contactUserId) {
+        return chatMessageRepository.findMessagesBetweenUsers(currentUserId, contactUserId);
     }
 }
