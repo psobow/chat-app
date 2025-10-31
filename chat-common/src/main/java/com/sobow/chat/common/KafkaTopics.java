97b09745eda890b1ac28ba8ce99d536eeb9f88f2
Patryk Sobow <patryk.sobow95@gmail.com>
2025-10-31 12:49:20 +0100

    Add constants with kafka topics names for message read and message read persisted

--- chat-common/src/main/java/com/sobow/chat/common/KafkaTopics.java
+++ chat-common/src/main/java/com/sobow/chat/common/KafkaTopics.java
@@ -5,6 +5,9 @@ public final class KafkaTopics {
     public static final String KAFKA_TOPIC_MESSAGE_SENT = "message.sent";
     public static final String KAFKA_TOPIC_MESSAGE_SENT_PERSISTED = "message.sent.persisted";
     
+    public static final String KAFKA_TOPIC_MESSAGE_READ = "message.read";
+    public static final String KAFKA_TOPIC_MESSAGE_READ_PERSISTED = "message.read.persisted";
+    
     private KafkaTopics() {
     }
 }
